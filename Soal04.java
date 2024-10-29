5b72f0819db1741ef55dcd1cdfe0098f78e9e436
Masefal Anjesa Loja <masefal.loja@gmail.com>
2024-10-29 17:45:59 +0700

    kumpulin

--- Soal04.java
+++ Soal04.java
@@ -1,6 +1,33 @@
+import java.util.Scanner;
+
 public class Soal04 {
 
 	public static void main(final String[] args) {
-		// Kerjakan soalnya di sini
+		Scanner input = new Scanner(System.in);
+		String[] pisah = input.nextLine().split(" ");
+		String kodeKerja = pisah[0];
+		int jamKerja = Integer.parseInt(pisah[1]);
+
+		input.close();
+	}
+	private static double gaji(String kodeKerja, int jamKerja) {
+		String kodekerja;
+		int honor= 30000;
+		double gaji = jamKerja * honor;
+
+		switch (kodekerja) {
+			case "DES":
+				gaji = gaji + 600000;
+			case "PRG":
+				jamKerja =  jamKerja * honor + 1200000;
+				gaji = gaji + 1200000;
+			case "WRT":
+				jamKerja =  jamKerja * honor + 400000;
+				gaji = gaji + 400000;
+			case "MKT":
+				jamKerja =  jamKerja * honor + 500000;
+				gaji = gaji + 500000;
+		}
+		if (jam kerja == 160) 
 	}
 }
